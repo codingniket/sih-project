16fe9e47c3baed7f901ede212872bdae02296692
AYUSH KUMAR <user@AKs-MacBook-Air.local>
2023-09-18 03:08:59 +0530

    Top Performers Section Added

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,22 @@
 import { UserButton } from "@clerk/nextjs";
 
-
 import React from "react";
 import Carousel from "./components/carousel";
 import Card from "./components/Card";
+import TopPerformer from "./components/TopPerformer";
 
 export default function Home() {
   return (
     <>
-    
-    <Carousel />
-    <div className=" text-3xl font-extrabold px-4 text-white">
-      <h1>Popular Coures</h1>
-    </div>
-    <Card className="py-10" />
+      <Carousel />
+      <div className=" text-3xl font-extrabold px-4 text-white">
+        <h1>Popular Courses</h1>
+      </div>
+      <Card className="py-10" />
+      <div className=" text-3xl font-extrabold px-4 text-white">
+        <h1>Top Performers</h1>
+      </div>
+      <TopPerformer />
     </>
-  )
+  );
 }
