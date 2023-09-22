6b23e3c928dda897e91615833034736823138294
codingniket <niketbachhawat7@gmail.com>
2023-09-22 11:22:13 +0530

    img error

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,6 +1,5 @@
-import Link from "next/link";
 import Image from "next/image";
-import Logo from "@/public/images/Team.png"
+import Logo from "../../public/images/Team.png";
 
 export default function Footer() {
   return (
