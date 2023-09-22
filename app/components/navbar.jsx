7763e40ea6b465153e4e80e633a5d618a0b6ac54
codingniket <niketbachhawat7@gmail.com>
2023-09-22 10:30:48 +0530

    ""

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -94,17 +94,5 @@ export default function navbar() {
         </div>
       </nav>
     </header>
-    // <div className="navbar bg-base-100 px-5">
-    // <div className="flex-1">
-    //     <Link href="/" className="text-white btn btn-ghost normal-case text-xl">Home</Link>
-    //     <Link href="/about" className="text-white btn btn-ghost normal-case text-xl">About</Link>
-    //     <Link href="/courses" className="text-white btn btn-ghost normal-case text-xl">Courses</Link>
-    // </div>
-    // <div className="flex-none gap-2">
-    //     <div className="dropdown dropdown-end">
-
-    //     </div>
-    // </div>
-    // </div>
   );
 }
