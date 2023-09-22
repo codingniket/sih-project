5b007394490625faf9398a10806f2b984cc70520
Sattwikee Ghosh <sattwikeeghosh95@gmail.com>
2023-09-22 17:09:43 +0530

    Update navbar.jsx

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -11,6 +11,14 @@ export const navItems = [
     url: "/courses"
     },
     {
+    name : "Skills",
+    url: "/skills"
+    },
+    {
+    name : "Extra Curricular",
+    url: "/extracurricular"
+    }
+    {
     name : "About",
     url: "/about"
     },
